5cf1b5a9ef4736211114471b4b2ac3fb2fb1844b
Thallapalli Krishna Sai Teja <154958662+ThallapalliKrishnaSaiTeja@users.noreply.github.com>
2025-03-07 22:11:55 +0530

    Add files via upload

---
+++ e-portfolio/ks.js
@@ -0,0 +1,8 @@
+function skills(){
+    let s = "Programming Languages: Python, Java, C#<br>Frontend: HTML, CSS, JavaScript<br>Developer Tools: VS Code <br>Soft Skills: Critical Thinking, Teamwork, Leadership<br>Coursework: Projects, Object Oriented Programming, Operating System, Data Structures"
+    document.getElementById("d").innerHTML = s;
+}
+function education(){
+    let e = "Institute Of Aeronautical Engineering<br>Computer Science Engineer(Cybersecurity) | Bachelor of Technology- BTech <br>CGPA: 8.98<br>Rajiv Gandhi University of Knowledge and Technology<br>Major in MPC and Minor in IT | Pre University course(PUC)<br>CGPA: 9.22<br>Kakatiya Group of Schools<br>SSC<br>CGPA: 10"
+    document.getElementById("d").innerHTML = e;
+}
