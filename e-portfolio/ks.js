7a77c8eff21ac6be39a3142825c768c61236e7c0
Thallapalli Krishna Sai Teja <154958662+ThallapalliKrishnaSaiTeja@users.noreply.github.com>
2025-03-09 10:34:15 +0530

    Update ks.js

--- e-portfolio/ks.js
+++ e-portfolio/ks.js
@@ -1,8 +1,8 @@
 function skills(){
-    let s = "Programming Languages: Python, Java, C#<br>Frontend: HTML, CSS, JavaScript<br>Developer Tools: VS Code <br>Soft Skills: Critical Thinking, Teamwork, Leadership<br>Coursework: Projects, Object Oriented Programming, Operating System, Data Structures"
+    let s = "Programming Languages: Python, Java, C#<br>Frontend: HTML, CSS, JavaScript<br>Developer Tools: VS Code <br>Soft Skills: Critical Thinking, Teamwork, Leadership<br>Coursework: Projects, Object Oriented Programming, Operating System, Data Structures";
     document.getElementById("d").innerHTML = s;
 }
 function education(){
-    let e = "Institute Of Aeronautical Engineering<br>Computer Science Engineer(Cybersecurity) | Bachelor of Technology- BTech <br>CGPA: 8.98<br>Rajiv Gandhi University of Knowledge and Technology<br>Major in MPC and Minor in IT | Pre University course(PUC)<br>CGPA: 9.22<br>Kakatiya Group of Schools<br>SSC<br>CGPA: 10"
+    let e = "Institute Of Aeronautical Engineering <span class = 'r'>Hyderabad, India</span><br>Computer Science Engineer(Cybersecurity) | Bachelor of Technology- BTech <span class = 'r'>Sep 2023 - Sep 2027</span> <br>CGPA: 8.98<br>Rajiv Gandhi University of Knowledge and Technology <span class = 'r'>Kadapa, India</span><br>Major in MPC and Minor in IT | Pre University course(PUC) <span class = 'r'>Aug 2021 - July 2023</span><br>CGPA: 9.22<br>Kakatiya Group of Schools <span class = 'r'>Hyderabad, India</span><br>SSC <span class = 'r'>May 2021</span><br>CGPA: 10";
     document.getElementById("d").innerHTML = e;
 }
